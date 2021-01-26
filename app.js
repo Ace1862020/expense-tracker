553d3977b1a772c1df5d17b2d724db454253abfb
Ace1862020 <kyds1862015@gmail.com>
2021-01-26 10:36:38 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const port = 3000
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/record', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
