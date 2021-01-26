41fce0779fb0522f3aaaf27ae4a1953b983843a3
Ace1862020 <kyds1862015@gmail.com>
2021-01-26 11:24:03 +0800

    feat: add record seeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,22 +1,13 @@
-const express = require('express')
 const mongoose = require('mongoose')
-const port = 3000
-
-const app = express()
+const Record = require('../record')
 
 mongoose.connect('mongodb://localhost/record', { useNewUrlParser: true, useUnifiedTopology: true })
+
 const db = mongoose.connection
+
 db.on('error', () => {
   console.log('mongodb error')
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
-
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`)
 })
