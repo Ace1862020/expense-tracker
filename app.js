f4d94bd18a061dde10e2aecf741706f5316e7908
Ace1862020 <kyds1862015@gmail.com>
2021-01-27 19:32:29 +0800

    feat: add new.handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const port = 3000
 
@@ -19,6 +20,9 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Index route
 app.get('/', (req, res) => {
   Record.find()
     .lean()
@@ -26,6 +30,20 @@ app.get('/', (req, res) => {
     .catch(error => {
       console.error(error => console.error(error))
     })
+})
+
+// Create route
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const { name, date, category, amount } = req.body
+  return Record.create({
+    name, date, category, amount
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
