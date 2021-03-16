d9dc78f53e1fa22caf6549c9901f72661d7d8a71
Ace1862020 <kyds1862015@gmail.com>
2021-03-16 11:00:25 +0800

    feat: add user model & add registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // Login
 router.get('/login', (req, res) => {
   res.render('login')
@@ -16,7 +18,18 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists.')
+      return res.render('register', { name, email, password, confirmPassword })
+    }
+    return User.create({
+      name, email, password
+    })
+      .then(() => res.redirect('login'))
+      .catch(error => console.log(error))
+  })
 })
 
 module.exports = router
