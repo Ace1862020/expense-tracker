9e78ace41c6a00682bca91046f40fe2fa4644682
Ace1862020 <kyds1862015@gmail.com>
2021-03-16 10:21:00 +0800

    feat: add user modules & login / register page

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/records', records)
+router.use('/', home) //首頁路由
+router.use('/records', records) //紀錄路由
+router.use('/users', users)
 
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+// Login
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+// Register
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+
+})
+
+module.exports = router
