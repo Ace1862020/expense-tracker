b3040976a39966451a7c2b07c3bb2aca94539d8a
Ace1862020 <kyds1862015@gmail.com>
2021-03-21 19:23:04 +0800

    modify recordSeeder & categorySeeder.

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,19 +1,31 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
 const Category = require('../category')
 const categoryList = require('../../public/json/category.json')
 
-
 db.once('open', () => {
   console.log('mongodb connected to the category!')
 
-  categoryList.forEach(data => {
-    Category.create({
-      category: data.category,
-      category_cn: data.category_cn,
-      icon: data.icon,
-      url: data.url
-    })
+  const categorys = []
+
+  categoryList.forEach(item => {
+    categorys.push(
+      Category.create({
+        category: item.category,
+        category_cn: item.category_cn,
+        icon: item.icon,
+        url: item.url
+      })
+    )
   })
+
+  Promise.all(categorys)
+    .then(() => {
+      db.close()
+    })
 
   console.log('Category done')
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,15 +1,56 @@
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
 const Record = require('../record')
-const recordList = require('../../public/json/record.json')
+const User = require('../user')
+const recordList = require('../../public/json/record.json').records
 
-db.once('open', () => {
+const SEED_USER = [
+  {
+    name: 'UserOne',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'UserTwo',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
+const count = 5
+
+db.once('open', async () => {
   console.log('mongodb connected to the record data!')
 
-  Record.create(recordList.records)
+  await new Promise((resolve) => {
+    SEED_USER.forEach((seeder, index) => {
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(seeder.password, salt))
+        .then(hash => User.create({
+          name: seeder.name,
+          email: seeder.email,
+          password: hash,
+        }))
 
-  console.log('Record done')
-  return db.close()
-})
-  .then(() => {
-    console.log('Record connection close.')
+        .then(user => {
+          const userId = user._id
+          return Promise.all(Array.from({ length: count }, async (_, i) => {
+            return Record.create({
+              ...recordList[i + index * count],
+              userId
+            })
+          }))
+            .then(() => {
+              console.log(`Record ${index + 1} connection done`)
+              if (index === SEED_USER.length - 1) resolve()
+            })
+        })
+    })
   })
+  process.exit()
+})
